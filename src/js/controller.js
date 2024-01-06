1f9d5c12fc82d9d20ab5302668208f711048ecb9
nexola <vitormatheusfv@gmail.com>
2024-01-06 17:49:36 -0300

    Criando objetos a partir de formulários

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import { searchView } from './views/searchView.js';
 import { resultsView } from './views/resultsView.js';
 import { paginationView } from './views/paginationView.js';
 import { bookmarksView } from './views/bookmarksView.js';
+import { addRecipeView } from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -79,6 +80,10 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -86,6 +91,7 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 
 init();
